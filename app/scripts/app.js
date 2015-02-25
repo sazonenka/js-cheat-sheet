0d7e603d13b6b7bba203d3abda0c3aa3b848220f
Aliaksandr Sazonenka <aliaksandr.sazonenka@gmail.com>
2015-02-24 22:28:01 -0800

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name jsCheatSheetApp
+ * @description
+ * # jsCheatSheetApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('jsCheatSheetApp', []);
